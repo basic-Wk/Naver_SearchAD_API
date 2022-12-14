95212dc11de33ace638bb6af33c279b587b1b714
Wonkyun <basic@Wonkyunui-MacBookAir.local>
2022-12-14 15:08:47 +0900

    네이버검색광고 캠페인 데이터 dash를 이용해 그리기 완료

--- app.py
+++ app.py
@@ -4,18 +4,15 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+from test import get_df
 
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = get_df()
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.bar(df, x="id", y={"클릭수","노출수"})
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-import time
-import random
-import requests
+import time, random, requests
 import pandas as pd
 from pandas import DataFrame
-
+from metrics import Naver_stat_metrics
 
 import signaturehelper
 import apikeys as api
-
 
 def get_header(method, uri, api_key, secret_key, customer_id):
     timestamp = str(round(time.time() * 1000))
@@ -23,68 +20,37 @@ CUSTOMER_ID = api.NaversearchADAPIkeys['CUSTOMER_ID']
 # Adcampaign Usage Sample
 
 # 1. GET adcampaign Usage Sample
+def get_df():
 
-uri = '/ncc/campaigns'
-method = 'GET'
-r = requests.get(BASE_URL + uri, headers=get_header(method, uri, API_KEY, SECRET_KEY, CUSTOMER_ID))
+    uri = '/ncc/campaigns'
+    method = 'GET'
+    r = requests.get(BASE_URL + uri, headers=get_header(method, uri, API_KEY, SECRET_KEY, CUSTOMER_ID))
 
-print("response status_code = {}".format(r.status_code))
-campaigns = r.json()
-campaign_list = []
-campaign_name = []
-for i in campaigns:
-    campaign = i.get('nccCampaignId')
-    campaign_n = i.get('name')
-    campaign_list.append(campaign)
-    campaign_name.append(campaign_n)
+    # print("response status_code = {}".format(r.status_code))
+    campaigns = r.json()
+    campaign_list = []
+    campaign_name = []
+    for i in campaigns:
+        campaign = i.get('nccCampaignId')
+        campaign_n = i.get('name')
+        campaign_list.append(campaign)
+        campaign_name.append(campaign_n)
 
-campaign_dict = { name: value for name, value in zip(campaign_list, campaign_name)}
-# Stat Usage Sample
+    campaign_dict = { name: value for name, value in zip(campaign_list, campaign_name)}
+    # Stat Usage Sample
 
-# 1. GET Summary Report per multiple entities 
+    # 1. GET Summary Report per multiple entities 
 
-uri = '/stats'
-method = 'GET'
-stat_ids = campaign_list
-r = requests.get(BASE_URL + uri, params={'ids': stat_ids, 'fields': '["impCnt", "clkCnt", "ctr", "cpc", "ccnt", "salesAmt", "crto", "cpConv"]', 'timeRange': '{"since":"2022-11-01","until":"2022-11-30"}'}, headers=get_header(method, uri, API_KEY, SECRET_KEY, CUSTOMER_ID))
+    uri = '/stats'
+    method = 'GET'
+    stat_ids = campaign_list
+    r = requests.get(BASE_URL + uri, params={'ids': stat_ids, 'fields': '["impCnt", "clkCnt", "ctr", "cpc", "ccnt", "salesAmt", "crto", "cpConv"]', 'timeRange': '{"since":"2022-11-01","until":"2022-11-30"}'}, headers=get_header(method, uri, API_KEY, SECRET_KEY, CUSTOMER_ID))
 
-fields = r.json()['data'][0].keys()
-df=DataFrame(None, columns=fields)
+    fields = r.json()['data']
+    df = pd.DataFrame(fields)
 
-for idx in range(0,len(r.json()['data'])):
-    df=pd.concat([df,DataFrame(r.json()['data'][idx],index=[idx])])
-
-df.set_index('id',inplace=True)
-df.rename({'ctr':'클릭률', 'clkCnt':'클릭수','cpc':'평균클릭비용','ccnt':'전환수','impCnt':'노출수', 'salesAmt':'총비용', 'crto':'전환율', 'cpConv':'전환당비용'}, axis=1, inplace=True)
-df.rename(campaign_dict, inplace=True)
-df=df[['노출수', '클릭수', '클릭률', '평균클릭비용', '전환수', '전환율', '총비용', '전환당비용']]
-print(df)
-
-
-# fields=r.json()['data'][0].keys()
-# df=DataFrame(None,columns=fields)
-
-# for idx in range(0,len(r.json()['data'])):
-#     df=df.append(DataFrame(r.json()['data'][idx],index=[idx]))
-
-# df.set_index('id',inplace=True)
-# df.rename({'ctr':'클릭률','convAmt':'전환매출액','clkCnt':'클릭수','cpc':'평균클릭비용','ccnt':'전환수','avgRnk':'평균노출순위','impCnt':'노출수','salesAmt':'총비용'},axis=1,inplace=True)
-# df=df[['노출수', '클릭수', '클릭률', '평균클릭비용', '총비용','전환수','전환매출액', '평균노출순위']]
-
-# df
-
-# impCnt          노출수    o
-# clkCnt          클릭수    o
-# ctr             클릭율    o
-# cpc             평균클릭비용  o
-# salesAmt        총비용    o
-# ccnt            전환수    o
-# crto            전환율    o
-# convAmt         전환매출액
-# ror             광고수익률(전환매출/총비용)
-# cpConv          전환당비용(총비용/전환수) o
-# avgRnk          평균노출순위
-# pcNxAvgRnk      PC 통검 평균노출순위
-# mblNxAvgRnk     모바일 통검 평균노출순위
-# recentAvgRnk    최근통검 평균노출순위
-# viewCnt         동영상조회수
+    # df.set_index('id',inplace=True)
+    df.rename(Naver_stat_metrics, axis=1, inplace=True)
+    df.replace(campaign_dict, inplace=True)
+    df=df[['id', '노출수', '클릭수', '클릭률', '평균클릭비용', '전환수', '전환율', '총비용', '전환당비용']]
+    return df
